cd5cff7a42d7aa6d016e7eeead12be9125023c21
Nxesyy <gesangrahmatullah@gmail.com>
2025-10-22 14:14:42 +0700

    crud entry points

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RentalModule } from './rental/rental.module';
 
 @Module({
-  imports: [],
+  imports: [RentalModule],
   controllers: [AppController],
   providers: [AppService],
 })
