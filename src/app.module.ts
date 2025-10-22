462c21c1951d7a8e2e1ff7175d96b786e9c02fef
Didaq <qurnaen_didaq_33rpl@student.smktelkom-mlg.sch.id>
2025-10-22 14:46:50 +0700

    prisma initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RentalModule } from './rental/rental.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [RentalModule],
+  imports: [RentalModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
